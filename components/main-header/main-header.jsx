a58e649560856a455cf61ab180460e56d6c589fa
matheus <matheuscosta.tech@gmail.com>
2024-10-13 10:15:11 -0300

    Fix typo

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -22,7 +22,7 @@ export default function MainHeader() {
               <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <NavLink href="/community">Browse Meals</NavLink>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
